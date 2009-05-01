1a42fcf2c17510d3d47c178d3649115c42b16944
Simone Carletti <weppos@weppos.net>
2009-05-01 16:06:35 +0200

    `Uninitialized constant RAILS_DEFAULT_LOGGER (NameError)' error message when running tests outside a Rails environment.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,12 +11,13 @@ require 'action_view/test_case'
 
 $:.unshift File.dirname(__FILE__) + '/../lib'
 
+RAILS_ROOT = '.'    unless defined? RAILS_ROOT
+RAILS_ENV  = 'test' unless defined? RAILS_ENV
+RAILS_DEFAULT_LOGGER = Logger.new(STDOUT) unless defined? RAILS_DEFAULT_LOGGER
+
 # simulate the inclusion as Rails does loading the init.rb file.
 require    'tabs_on_rails'
 require    File.dirname(__FILE__) + '/../init.rb'
-
-RAILS_ROOT = '.'    unless defined? RAILS_ROOT
-RAILS_ENV  = 'test' unless defined? RAILS_ENV
 
 ActionController::Base.logger = nil
 ActionController::Routing::Routes.reload rescue nil
