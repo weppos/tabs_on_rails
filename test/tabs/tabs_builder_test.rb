39eb14f00ee4fd0d13df0e75b81e9a5bc96cef7b
Simone Carletti <weppos@weppos.net>
2010-08-09 15:59:29 +0200

    Cleanup tests

--- test/tabs/tabs_builder_test.rb
+++ test/tabs/tabs_builder_test.rb
@@ -20,7 +20,12 @@ class TabsBuilderTest < ActionView::TestCase
 
   def setup
     @template = Template.new
-    @builder  = TabsOnRails::Tabs::TabsBuilder.new(@template)
+    @klass    = TabsOnRails::Tabs::TabsBuilder
+    @builder  = @klass.new(@template)
+  end
+
+  def test_should_extend_builder
+    assert_equal TabsOnRails::Tabs::Builder, @klass.superclass
   end
 
 
@@ -41,18 +46,14 @@ class TabsBuilderTest < ActionView::TestCase
   end
 
 
-  def test_should_implement_builder
-    assert_equal(TabsOnRails::Tabs::Builder, TabsOnRails::Tabs::TabsBuilder.superclass)
-  end
-
   def test_tab_for_should_return_link_to_unless_current_tab
-    assert_dom_equal('<li><a href="#">Welcome</a></li>', @builder.tab_for(:welcome, 'Welcome', '#'))
-    assert_dom_equal('<li><a href="http://foobar.com/">Foo Bar</a></li>', @builder.tab_for(:welcome, 'Foo Bar', 'http://foobar.com/'))
+    assert_dom_equal '<li><a href="#">Welcome</a></li>', @builder.tab_for(:welcome, 'Welcome', '#')
+    assert_dom_equal '<li><a href="http://foobar.com/">Foo Bar</a></li>', @builder.tab_for(:welcome, 'Foo Bar', 'http://foobar.com/')
   end
 
   def test_tab_for_should_return_span_if_current_tab
-    assert_dom_equal('<li><span>Dashboard</span></li>', @builder.tab_for(:dashboard, 'Dashboard', '#'))
-    assert_dom_equal('<li><span>Foo Bar</span></li>', @builder.tab_for(:dashboard, 'Foo Bar', '#'))
+    assert_dom_equal '<li><span>Dashboard</span></li>', @builder.tab_for(:dashboard, 'Dashboard', '#')
+    assert_dom_equal '<li><span>Foo Bar</span></li>', @builder.tab_for(:dashboard, 'Foo Bar', '#')
   end
 
 end
