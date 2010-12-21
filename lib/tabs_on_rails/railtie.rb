d88850a61aa86841d8bcccd775513286cdbdb2f2
Simone Carletti <weppos@weppos.net>
2010-12-21 16:09:52 +0100

    Remove Rails 2 hacks and make it compatibile with Rails 3 only

--- lib/tabs_on_rails/railtie.rb
+++ lib/tabs_on_rails/railtie.rb
@@ -13,32 +13,16 @@
 #
 #++
 
+require 'action_controller/base'
 
 module TabsOnRails
 
-  if defined? Rails::Railtie
-    require "rails"
-
-    class Railtie < Rails::Railtie
-      initializer "tabs_on_rails.initialize" do
-        ActiveSupport.on_load :action_controller do
-          TabsOnRails::Railtie.init
-        end
+  class Railtie < ::Rails::Railtie
+    initializer "tabs_on_rails.initialize" do
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.send :include, TabsOnRails::ControllerMixin
       end
     end
-
-  end
-
-  class Railtie
-
-    def self.rails_version
-      @@rails_version ||= Rails::VERSION::STRING rescue "2.2"
-    end
-
-    def self.init
-      ActionController::Base.send :include, TabsOnRails::ControllerMixin
-    end
-
   end
 
 end
