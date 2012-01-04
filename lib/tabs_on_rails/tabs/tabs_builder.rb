9bd783a805f30553df69c94818d12f8b9011d03f
Ace Suares <ace@suares.an>
2012-01-04 19:46:57 -0400

    remote test3

--- lib/tabs_on_rails/tabs/tabs_builder.rb
+++ lib/tabs_on_rails/tabs/tabs_builder.rb
@@ -45,7 +45,7 @@ module TabsOnRails
       # Implements Builder#tab_for.
       #
       def tab_for(tab, name, options, item_options = {})
-        remote = item_options.delete :remote
+        remote = item_options.delete :remote || false
         item_options[:class] = item_options[:class].to_s.split(" ").push("current").join(" ") if current_tab?(tab)
         content = @context.link_to_unless(current_tab?(tab), name, options, :remote => remote) do
           @context.content_tag(:span, name)
